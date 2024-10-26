01e3437bb1fb6d301fab1301948664fc6acfcf20
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2024-10-26 16:49:51 +0200

    Add dependency injection

---
+++ SpotifyToAppleMusicPlaylistConverter/DependencyInjection/DependencyInjectionConfigurationBuilder.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using SpotifyAPI.Web;
+using SpotifyToAppleMusicPlaylistConverter.Configuration;
+
+namespace SpotifyToAppleMusicPlaylistConverter.DependencyInjection
+{
+    internal static class DependencyInjectionConfigurationBuilder
+    {
+        public static IContainer Build()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<SpotifyClient>().As<ISpotifyClient>();
+            builder.RegisterType<ApplicationConfig>().AsSelf();
+
+            return builder.Build();
+        }
+    }
+}
